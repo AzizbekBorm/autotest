09a99f02ab5eb38b31860b339fb914e84e65ef36
Azizbek Borm <azizbek1084@gmail.com>
2016-07-19 16:44:53 +0300

    add newName

--- src/com/Azizbek/runners/CircleAreaRunner.java
+++ src/com/Azizbek/runners/CircleAreaRunner.java
@@ -1,9 +1,11 @@
-package com.Azizbek.utils.convertors;
+package com.Azizbek.runners;
+
+import com.Azizbek.utils.convertors.CircleArea;
 
 /**
  * Created by Yarik on 16.07.2016.
  */
-public class RunnerCircleArea {
+public class CircleAreaRunner {
     public static void main(String args[]) {
         CircleArea.calculate();
     }
--- src/com/Azizbek/runners/PrimitiveConvertorRunner.java
+++ src/com/Azizbek/runners/PrimitiveConvertorRunner.java
@@ -1,9 +1,11 @@
-package com.Azizbek.utils.convertors;
+package com.Azizbek.runners;
+
+import com.Azizbek.utils.convertors.PrimitiveConvertor;
 
 /**
  * Created by Yarik on 16.07.2016.
  */
-public class Executor {
+public class PrimitiveConvertorRunner {
     public static void main(String args[]) {
     PrimitiveConvertor primitiveConvertor = new PrimitiveConvertor();
         primitiveConvertor.floatToChar();
